97bd778b41f5daeb6dd4802451ea8751030f95ac
Warakorn Mukdaprasert <warakorn.m@forth.co.th>
2023-09-16 19:28:01 +0700

    add xcode project folder

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Warakorn Mukdaprasert on 16/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
